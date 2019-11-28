3e8eafee6f7f17e85986e7c2091427b72cfdeeb5
Shiraj Ganguly <Shiraj.ganguly@atlasobscura.com>
2019-11-28 13:23:50 -0700

    Add a basic endpoint for getting a thumbnail from an s3 asset

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 require('dotenv').config();
+const thumbnailWidth = 200;
+const thumbnailHeight = 150;
+const Jimp = require('jimp');
 const express = require('express')
 const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello World!'));
+const bucketParams = {
+  Bucket: "shirajganguly.com",
+}
+
+app.get('/thumbnail/:image_id', (req, res) =>{
+  s3.getObject({...bucketParams, ...{Key: `images/${req.params.image_id}`}},(error, data) => {
+    if(!error) {
+      var imageData = Buffer.from(data.Body);
+      Jimp.read(imageData)
+          .then(image => {
+              image.cover(thumbnailWidth, thumbnailHeight)
+              .getBufferAsync(Jimp.AUTO)
+              .then((imageBuffer) => {
+                res.write(imageBuffer);
+                res.send();
+              })
+          });
+    } else {
+      console.error(error);
+    }
+  })
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
